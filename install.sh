96b474fdbd86687b754fbcb3ae3b943609c296ca
Samuel Shepard <vfn4@cdc.gov>
2026-04-14 11:53:19 -0400

    Improve installer and fix pathing bug

--- install.sh
+++ install.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+
+command -v "cargo" > /dev/null 2>&1 || {
+    echo "ERROR: you may need to install Rust nightly!"
+    exit 1
+}
+
+if [ -n "$1" ]; then
+    if grep -Eq "not a recognized processor" <(rustc -C "target-cpu=$1" --print host-tuple 2>&1); then
+        echo "WARNING: cannot find target-cpu '$1', skipping"
+    else
+        export RUSTFLAGS="${RUSTFLAGS:+$RUSTFLAGS }-C target-cpu=$1"
+    fi
+fi
+
 cargo build --profile prod \
-    && cp target/release/sswsort . \
-    || echo "You may need to install Rust nightly!"
+    && cp target/prod/sswsort . \
+    || {
+        echo "ERROR: Installation failed!"
+        exit 1
+    }
